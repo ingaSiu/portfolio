fb770efd2a6c1c4a5bd7a51fa9cc1cc3abd02887
ingaSiu <ingasiud@gmail.com>
2023-05-24 21:14:56 +0300

    main layout added

---
+++ src/Layout/MainLayout.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react';
+
+const MainLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <div>{children}</div>
+    </>
+  );
+};
+
+export default MainLayout;
