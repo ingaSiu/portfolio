dad98af504e740f4b13fe9e0c605e84ee9235b18
ingaSiu <ingasiud@gmail.com>
2023-05-26 20:20:31 +0300

    layout added

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,9 +1,13 @@
+import Footer from '../components/Footer/Footer';
+import Navbar from '../components/Navbar/Navbar';
 import { PropsWithChildren } from 'react';
 
 const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
+      <Navbar />
       <div>{children}</div>
+      <Footer />
     </>
   );
 };
