91f7c5ba760cc80af965f9adc577d6462bf03d96
ingaSiu <ingasiud@gmail.com>
2023-05-28 16:20:56 +0300

    about component structure

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,35 @@
+import Skill from './Skill';
+import styles from './About.module.scss';
+
 const About = () => {
-  return <div id="about">About</div>;
+  return (
+    <section id="about" className={styles.about_container}>
+      <div className={styles.about}>
+        <h1>ABOUT ME</h1>
+        <p>Trumpas aprasymas apie mane</p>
+        <p>savybes aprasymas</p>
+        <p>savybes aprasymas</p>
+        <p>savybes aprasymas</p>
+        <p>savybes aprasymas</p>
+      </div>
+
+      <div className={styles.skills}>
+        <h1>MY SKILLS</h1>
+        <div className={styles.main_skills}>
+          <div className={styles.col}>
+            <Skill img="JavaScript.svg" name="JavaScript" />
+          </div>
+          <div className={styles.col}>
+            <Skill img="TypeScript.svg" name="TypeScript" />
+          </div>
+        </div>
+        <div className={styles.additional_skills}>
+          <h4>Familiar with:</h4>
+          <p>add some icons?</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
