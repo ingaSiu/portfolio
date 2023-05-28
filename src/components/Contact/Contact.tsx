fcca99438a99a56a050d42d42ee9cc4aca5ce4c9
ingaSiu <ingasiud@gmail.com>
2023-05-28 19:17:01 +0300

    contact component structure

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,5 +1,39 @@
+import { FaEnvelope, FaGithub, FaLinkedin, FaMapMarkerAlt, FaMobileAlt } from 'react-icons/fa';
+
+import styles from './Contact.module.scss';
+
 const Contact = () => {
-  return <div id="contact">Contact</div>;
+  return (
+    <section id="contact" className={styles.container}>
+      <h1>Contact Me</h1>
+      <div className={styles.wrapper}>
+        <div>CONTACT ME FORM</div>
+        <div>
+          <div>
+            <h3>CONTACT INFO</h3>
+            <div>
+              <FaEnvelope />
+              ingasiu@gmail.com
+            </div>
+            <p>
+              <FaMobileAlt /> +370 604 66592
+            </p>
+            <p>
+              <FaMapMarkerAlt />
+              Vilnius, Lithuania
+            </p>
+          </div>
+          <div>
+            <h3>Social Networks</h3>
+            <div>
+              <FaGithub />
+              <FaLinkedin />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
