8950ca7627d0b3cd52e086b6f9f382a6b954f48f
ingaSiu <ingasiud@gmail.com>
2023-05-28 18:19:36 +0300

    button component created

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,11 @@
-const Button = () => {
-  return <div>Button</div>;
+import styles from './Button.module.scss';
+
+type ButtonProps = {
+  name: string;
+};
+
+const Button = ({ name }: ButtonProps) => {
+  return <button className={styles.btn}>{name}</button>;
 };
 
 export default Button;
