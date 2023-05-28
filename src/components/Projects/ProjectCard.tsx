c5514d0483c2119b7e516b4cedf89070fa5e884c
ingaSiu <ingasiud@gmail.com>
2023-05-28 19:17:45 +0300

    project card structure added

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,5 +1,22 @@
-const ProjectCard = () => {
-  return <div>ProjectCard</div>;
+import Button from '../Button/Button';
+import styles from './ProjectCard.module.scss';
+
+type CardProps = {
+  name: string;
+  img: string;
+};
+
+const ProjectCard = ({ name, img }: CardProps) => {
+  return (
+    <div className={styles.container}>
+      <h3>{name}</h3>
+      <img src={img} alt={name} />
+      <div className={styles.btn_container}>
+        <Button name="ABOUT" />
+        <Button name="DEMO" />
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
