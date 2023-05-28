d42ac300a1c090a570a975eba9a55fe0622a3949
ingaSiu <ingasiud@gmail.com>
2023-05-28 18:19:48 +0300

    home page structure

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,14 @@
 import About from '../../components/About/About';
+import Contact from '../../components/Contact/Contact';
+import Projects from '../../components/Projects/Projects';
 
 const HomePage = () => {
   return (
     <div>
       <h1>My HomePage</h1>
       <About />
+      <Projects />
+      <Contact />
     </div>
   );
 };
