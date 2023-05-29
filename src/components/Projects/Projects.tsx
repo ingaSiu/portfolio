23c020375aab07266eea545321bf633a015a0767
ingaSiu <ingasiud@gmail.com>
2023-05-29 19:15:48 +0300

    project card mapping

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,10 +1,16 @@
 import ProjectCard from './ProjectCard';
+import { projectInfo } from '../../assets/project_info/project_info';
+import styles from './Projects.module.scss';
 
 const Projects = () => {
   return (
     <section>
       <h1>My Projects</h1>
-      <ProjectCard name="Food blog" img="JavaScript.svg" />
+      <div className={styles.cards_wrapper}>
+        {projectInfo.map((item) => (
+          <ProjectCard key={item.name} name={item.name} img={item.img} url={item.url} />
+        ))}
+      </div>
     </section>
   );
 };
