dbef606113f42bfd8fe81c378a739b583febd805
ingaSiu <ingasiud@gmail.com>
2023-05-30 10:28:05 +0300

    mock form added

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,5 +1,6 @@
 import { FaEnvelope, FaGithub, FaLinkedin, FaMapMarkerAlt, FaMobileAlt } from 'react-icons/fa';
 
+import Button from '../Button/Button';
 import styles from './Contact.module.scss';
 
 const Contact = () => {
@@ -7,7 +8,24 @@ const Contact = () => {
     <section id="contact" className={styles.container}>
       <h1>Contact Me</h1>
       <div className={styles.wrapper}>
-        <div>CONTACT ME FORM</div>
+        <div className={styles.form_container}>
+          <div>
+            <label for="name">Your name</label>
+            <input type="text" name="name"></input>
+          </div>
+
+          <div>
+            <label for="message">Message</label>
+            <textarea name="message" />
+          </div>
+
+          <div>
+            <label for="email">Your Email</label>
+            <input type="email" name="email"></input>
+          </div>
+
+          <Button name="Contact Me!" />
+        </div>
         <div>
           <div>
             <h3>CONTACT INFO</h3>
