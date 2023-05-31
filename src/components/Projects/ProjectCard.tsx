a286e344a6bf382e404f10114f4445980f310335
ingaSiu <ingasiud@gmail.com>
2023-05-31 20:45:15 +0300

    btn to project card

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -4,16 +4,22 @@ import styles from './ProjectCard.module.scss';
 type CardProps = {
   name: string;
   img: string;
+  url: string;
+  id?: number;
 };
 
-const ProjectCard = ({ name, img }: CardProps) => {
+const ProjectCard = ({ name, img, url }: CardProps) => {
   return (
     <div className={styles.container}>
       <h3>{name}</h3>
       <img src={img} alt={name} />
       <div className={styles.btn_container}>
-        <Button name="ABOUT" />
-        <Button name="DEMO" />
+        <Button name="ABOUT">
+          <a href=""></a>
+        </Button>
+        <Button name="DEMO">
+          <a href={url} target="_blank"></a>
+        </Button>
       </div>
     </div>
   );
