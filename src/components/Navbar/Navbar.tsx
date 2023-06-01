3ef093132180e443c91c9ba32149ceb3eb407e54
ingaSiu <ingasiud@gmail.com>
2023-06-01 21:51:43 +0300

    added different nav sections

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,41 +1,67 @@
+import { ABOUT_PATH, CONTACT_PATH, PROJECTS_PATH } from '../../routes/consts';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+import { Link } from 'react-router-dom';
 import styles from './Navbar.module.scss';
 import { useState } from 'react';
 
 const Navbar = () => {
   const [extendNavbar, setExtendNavbar] = useState(false);
 
+  const handleNav = () => {
+    setExtendNavbar(!extendNavbar);
+  };
+
   return (
-    <div className={styles.navbar_container} extendNavbar={extendNavbar}>
-      <div className={styles.inner_container}>
+    <nav className={styles.navbar}>
+      {/* normal navbar section */}
+      <div className={styles.navbar_wrapper}>
         <div className={styles.left_container}>
           <img src="./cat_icon.png" />
           <p>&#x3c; Inga Siudikiene &#x2215;&#x3e;</p>
         </div>
-
         <div className={styles.right_container}>
           <div className={styles.links_container}>
-            <a href="#about">About</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact">Contact Me</a>
-            <button
-              className={styles.nav_btn}
+            <Link to={ABOUT_PATH} onClick={handleNav}>
+              About
+            </Link>
+            <Link to={PROJECTS_PATH} onClick={handleNav}>
+              Projects
+            </Link>
+            <Link to={CONTACT_PATH} onClick={handleNav}>
+              Contact Me
+            </Link>
+            <div
+              className={styles.navbar_hamburger}
               onClick={() => {
                 setExtendNavbar((current) => !current);
               }}
             >
-              {extendNavbar ? <>&#10005;</> : <>&#8801;</>}
-            </button>
+              {extendNavbar ? <FaTimes /> : <FaBars />}
+            </div>
           </div>
         </div>
-        {extendNavbar && (
-          <div className={styles.nav_extend}>
-            <a href="#about">About</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact">Contact Me</a>
-          </div>
-        )}
       </div>
-    </div>
+
+      {/* mobile menu section */}
+      <div className={`${styles.nav_extend_menu} ${extendNavbar ? styles.nav_extend_open : ''}`}>
+        <div onClick={handleNav} className={styles.nav_extend_close}>
+          <FaTimes />
+        </div>
+
+        <div className={styles.nav_extend_links}>
+          <Link to={ABOUT_PATH} onClick={handleNav}>
+            About
+          </Link>
+          <Link to={PROJECTS_PATH} onClick={handleNav}>
+            Projects
+          </Link>
+          <Link to={CONTACT_PATH} onClick={handleNav}>
+            Contact Me
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
 };
 
