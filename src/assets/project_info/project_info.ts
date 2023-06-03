4df2124012d29848026f786eea28ab62b9b2edb6
ingaSiu <ingasiud@gmail.com>
2023-06-03 21:04:40 +0300

    experiment with info

---
+++ src/assets/project_info/project_info.ts
@@ -0,0 +1,21 @@
+type InfoProps = {
+  name: string;
+  img: string;
+  id: number;
+  url: string;
+};
+
+export const projectInfo: InfoProps[] = [
+  {
+    name: 'First ProjectCard',
+    img: './assets/project_img/Project_1_main.png',
+    id: 1,
+    url: 'https://food-blog.ingasiu.online/#/',
+  },
+  {
+    name: 'Second Project',
+    img: 'Project_1_form',
+    id: 2,
+    url: 'https://github.com/ingaSiu/food-blog-api ',
+  },
+];
