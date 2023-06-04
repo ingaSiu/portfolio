8764b7ac2353cbeb2576fa3d21ba67708caf8114
ingaSiu <ingasiud@gmail.com>
2023-06-04 18:13:07 +0300

    project card with all props

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,19 +1,29 @@
 import Button from '../Button/Button';
+import { Link } from 'react-router-dom';
 import styles from './ProjectCard.module.scss';
 
 type CardProps = {
   name: string;
   img: string;
+  urlDemo: string;
+  urlGit: string;
+  id?: number;
+  description: string;
 };
 
-const ProjectCard = ({ name, img }: CardProps) => {
+const ProjectCard = ({ name, img, urlDemo, urlGit, description }: CardProps) => {
   return (
     <div className={styles.container}>
       <h3>{name}</h3>
       <img src={img} alt={name} />
+      <p>{description}</p>
       <div className={styles.btn_container}>
-        <Button name="ABOUT" />
-        <Button name="DEMO" />
+        <Link to={urlDemo} target="_blank">
+          <Button name="DEMO" />
+        </Link>
+        <Link to={urlGit} target="_blank">
+          <Button name="GITHUB" />
+        </Link>
       </div>
     </div>
   );
