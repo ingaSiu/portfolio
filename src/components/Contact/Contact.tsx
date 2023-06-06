83d10d2043129fafa39b66851ac6d599535ff26f
ingaSiu <ingasiud@gmail.com>
2023-06-06 14:51:42 +0300

    made contact component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,55 +1,14 @@
-import { FaEnvelope, FaGithub, FaLinkedin, FaMapMarkerAlt, FaMobileAlt } from 'react-icons/fa';
-
-import Button from '../Button/Button';
+import ContactForm from './ContactForm';
+import ContactInfo from './ContactInfo';
 import styles from './Contact.module.scss';
 
 const Contact = () => {
   return (
     <section id="contact" className={styles.container}>
       <h1>Contact Me</h1>
-      <div className={styles.wrapper}>
-        <div className={styles.form_container}>
-          <div>
-            <label for="name">Your name</label>
-            <input type="text" name="name"></input>
-          </div>
 
-          <div>
-            <label for="message">Message</label>
-            <textarea name="message" />
-          </div>
-
-          <div>
-            <label for="email">Your Email</label>
-            <input type="email" name="email"></input>
-          </div>
-
-          <Button name="Contact Me!" />
-        </div>
-        <div>
-          <div>
-            <h3>CONTACT INFO</h3>
-            <div>
-              <FaEnvelope />
-              ingasiu@gmail.com
-            </div>
-            <p>
-              <FaMobileAlt /> +370 604 66592
-            </p>
-            <p>
-              <FaMapMarkerAlt />
-              Vilnius, Lithuania
-            </p>
-          </div>
-          <div>
-            <h3>Social Networks</h3>
-            <div>
-              <FaGithub />
-              <FaLinkedin />
-            </div>
-          </div>
-        </div>
-      </div>
+      <ContactForm />
+      <ContactInfo />
     </section>
   );
 };
