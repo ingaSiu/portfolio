5c7426ddbb00039a58d788be1883f0baffd66229
ingaSiu <ingasiud@gmail.com>
2023-06-07 21:00:15 +0300

    added hero section to home

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
 import About from '../../components/About/About';
 import Contact from '../../components/Contact/Contact';
+import Hero from '../../components/Hero/Hero';
 import Projects from '../../components/Projects/Projects';
+import style from './HomePage.module.scss';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>My HomePage</h1>
+    <div className={style.home_wrapper}>
+      <Hero />
       <About />
       <Projects />
       <Contact />
