f9b5b7b5908ab142afddb268f8dd0863d4346e5f
ingaSiu <ingasiud@gmail.com>
2023-06-08 17:40:46 +0300

    fix

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,11 +2,10 @@ import About from '../../components/About/About';
 import Contact from '../../components/Contact/Contact';
 import Hero from '../../components/Hero/Hero';
 import Projects from '../../components/Projects/Projects';
-import style from './HomePage.module.scss';
 
 const HomePage = () => {
   return (
-    <div className={style.home_wrapper}>
+    <div>
       <Hero />
       <About />
       <Projects />
