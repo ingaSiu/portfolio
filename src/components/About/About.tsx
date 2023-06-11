c7bc53307c215c8607063be44b77337f1c41870f
ingaSiu <ingasiud@gmail.com>
2023-06-11 14:09:22 +0300

    some spelling mistakes fixed

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -26,20 +26,20 @@ const About = () => {
           concepts and tools for front-end programming.
         </p>
         <p>
-          <span>Positive attitude saves the day!</span> In programming everyday is challenging and sometimes it can be
-          frustrating. But I think that having a positive attitude in programming is essential for success. A positive
-          mindset fosters creativity, encourages collaboration, and promotes a growth-oriented mindset. So even in
-          difficult times I can reset myself, reflect about my frustrations and take the challenge again.
+          <span>Positive attitude saves the day!</span> I think that having a positive attitude in programming is
+          essential for success. A positive mindset fosters creativity, encourages collaboration, and promotes a
+          growth-oriented mindset. So even in difficult times I can reset myself, reflect about my frustrations and take
+          the challenge on again.
         </p>
         <p>
           <span>Reflection and Analysis are your friends!</span> I believe that front-end programming is very dynamic so
           having a solid understanding of its priciples is a must.That's why I analyze various web pages, apps,
           components and try to recreate them. Not only it helps to expand my knowledge about user-centric
-          approach,responsive design, accessibility, but also it lets me to reflect on my knowledge, skills and
+          approach,responsive design, accessibility, etc., but also it lets me to reflect on my knowledge, skills and
           encourages me to study and practice even more.
         </p>
         <p>
-          <span>Googling is better than procrastination</span> Then a problem arises I always try to find more
+          <span>Googling is better than procrastination!</span> Then a problem arises I always try to find more
           information and put aquired knowledge to use by creating a solution. So StackOverflow, Youtube, ChatGTP, etc.
           are my companions in the programming journey.
         </p>
